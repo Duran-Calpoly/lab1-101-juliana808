7469d6c5a705ed1334faeb63e292ccfbc926ef88
Juliana Metzler-Gottlieb <60299521+juliana808@users.noreply.github.com>
2026-01-13 00:29:53 +0000

    Completed up to day 4

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,19 @@
-pass
+num1 = 15.8
+num2 = 3
+num3 = 8.4
+def calculate_average(num1, num2, num3):
+    return (float(num1) + num2 + float(num3)) / 3.0
+print(calculate_average(num1, num2, num3))
+
+
+number = 85.60
+def add_tax(number):
+    return number * 1.10 
+print(add_tax(number))
+
+
+name = "Juli"
+def greet_user(name):
+    return "Hello " + name
+
+print(greet_user(name))
