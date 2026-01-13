1370967fd2807f5eac8b7e8769c7e92b142b4946
Juliana Metzler-Gottlieb <60299521+juliana808@users.noreply.github.com>
2026-01-13 03:51:05 +0000

    Completed Lab 1

--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,24 @@
-pass
+def check_multiple(number):
+    if  number % 5 == 0 and number % 3 == 0:
+        return True
+    else: 
+        return False
+
+
+secret_word = "Python123"
+def check_password(input_string):
+    if input_string == secret_word:
+        return "access granted"
+    else:
+        return "access denied"
+    
+
+def calculate_federal_tax(salary):
+    if salary <= 11000:
+        return salary * 0.1
+    elif salary <= 44725:
+        return  salary * 0.12
+    elif salary <= 95375:
+        return salary * 0.22
+    else:
+        return salary * 0.24
